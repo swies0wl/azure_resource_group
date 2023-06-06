44e1e5dd620dfbd91e36a9b2923df1f2a41064cf
swies0wl <77213405+swies0wl@users.noreply.github.com>
2023-06-06 11:05:26 -0400

    test update

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
+resource "azurerm_resource_group" "this" {
+  name     = var.name
+  location = var.location
+  tags     = var.tags
+}
 
+output resource_group_name {
+  value = resource.azurerm_resource_group.this.name
+}
+
+output resource_group_location {
+  value = resource.azurerm_resource_group.this.location
+}
