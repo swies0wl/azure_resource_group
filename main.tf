efb220b9ef71e7135af1007c72b11bad9ef8ed4e
swies0wl <77213405+swies0wl@users.noreply.github.com>
2025-05-12 10:06:13 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-#provider "azurerm" {
-#  features {}
-#}
+provider "azurerm" {
+  features {}
+}
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
 resource "azurerm_resource_group" "this" {
